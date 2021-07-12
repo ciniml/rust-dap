9a0aab7ea3808ef4e9ade16cadd79fafaa571f39
Kenta IDA <fuga@fugafuga.org>
2021-07-13 06:38:16 +0900

    Add LICENSE and fix formatting.

--- rust-dap-xiao/src/main.rs
+++ rust-dap-xiao/src/main.rs
@@ -1,3 +1,19 @@
+// Copyright 2021 Kenta Ida
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #![no_std]
 #![no_main]
 
@@ -372,7 +388,7 @@ fn main() -> ! {
                 .manufacturer("fugafuga.org")
                 .product("CMSIS-DAP")
                 .serial_number("test")
-                .device_class(USB_CLASS_MISCELLANEOUS) 
+                .device_class(USB_CLASS_MISCELLANEOUS)
                 .device_class(USB_SUBCLASS_COMMON)
                 .device_protocol(USB_PROTOCOL_IAD)
                 .composite_with_iads()
